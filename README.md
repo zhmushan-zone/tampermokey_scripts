c478c56660ab282a8c78c95b6ea6a7911482823f
zhmushan <zhmushan@qq.com>
2020-02-01 15:07:47 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tampermokey_scripts
+
+- Coursera subtitles(outside the video)
+  - [Greasy Fork](https://greasyfork.org/en/scripts/383424-coursera-subtitles-outside-the-video)
+  - [Source](./coursera_subtitles.ts)
---
+++ dist/coursera_subtitles.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Coursera subtitles(outside the video)
+// @description  Coursera subtitles outside the video.
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @author       木杉
+// @include      http://www.coursera.org/*
+// @include      https://www.coursera.org/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+(function () {
+    const contentStyle = `
+    color: black;
+    font-size: 18px;
+    line-height: 25px;
+    background: beige;
+    padding: 12px;
+  `;
+    function main(target, panel) {
+        panel.style.justifyContent = "center";
+        const content = document.createElement("div");
+        content.style.cssText = contentStyle;
+        panel.innerHTML = "";
+        panel.append(content);
+        if (target && panel && document.querySelector("video")) {
+            const options = {
+                attributes: true,
+                attributeFilter: ["class"],
+                childList: true,
+                subtree: true
+            };
+            const mb = new MutationObserver(() => {
+                const active = target.querySelector(".active");
+                content.innerHTML = active.innerHTML;
+            });
+            mb.observe(target, options);
+        }
+    }
+    const interval = setInterval(() => {
+        const target = document.querySelector(".rc-Transcript");
+        const panel = document.querySelector(".rc-VideoToolbar.horizontal-box.align-items-spacebetween");
+        if (target && panel) {
+            main(target, panel);
+            window.clearInterval(interval);
+        }
+    }, 500);
+})();
